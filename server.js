90507c43ebeb63fb490b0ca416841b1bff1311cc
Nicholas <danielnick210@gmail.com>
2026-01-05 09:58:49 +0100

    i created a .env file to read the port as fallback just in case shit hits the roof

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+import "dotenv/config"
 import app from './src/app.js';
 
-const PORT = 3000;
+// process.env.PORT reads the value from your .env file
+// We keep 3000 as a "fallback" just in case the .env is missing
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
