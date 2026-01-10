a1c01acf346df65a54b5b08bd3abd8189e80dfba
Nicholas <danielnick210@gmail.com>
2026-01-10 12:28:23 +0100

    bug fixes on the db file

--- src/db/db.js
+++ src/db/db.js
@@ -1,25 +1,25 @@
 import knex from 'knex';
 
 const db = knex({
-  client: 'sqlite3',
-  connection: {
-	filename: './dev.sqlite3' // This creates a file in your project folder
-  },
-  useNullAsDefault: true
+	client: 'sqlite3',
+	connection: {
+		filename: './dev.sqlite3' // This creates a file in your project folder
+	},
+	useNullAsDefault: true
 });
 
 // This function creates the table if it doesn't exist
 export const initDb = async () => {
-  const hasTable = await db.schema.hasTable('notes');
-  if (!hasTable) {
-	await db.schema.createTable('notes', (table) => {
-	  table.increments('id').primary(); // Auto-incrementing ID
-	  table.string('title').notNullable();
-	  table.text('content');
-	  table.timestamp('createdAt').defaultTo(db.fn.now());
-	});
-	console.log("Database & Notes table created!");
-  }
+	const hasTable = await db.schema.hasTable('notes');
+	if (!hasTable) {
+		await db.schema.createTable('notes', (table) => {
+			table.increments('id').primary(); // Auto-incrementing ID
+			table.string('title').notNullable();
+			table.text('content');
+			table.timestamp('createdAt').defaultTo(db.fn.now());
+		});
+		console.log("Database & Notes table created!");
+	}
 };
 
 export default db;
